e67d4c78c251b9e3620fb7ef1cc3f922e46fe1b3
stephenlasky <stephenjlasky@gmail.com>
2017-10-02 14:27:00 -0400

    Initial commit.

---
+++ prob_tracking/getKeypoints.m
@@ -0,0 +1,11 @@
+function [keyXs, keyYs] = getKeypoints(im, tau)
+% Detecting keypoints using Harris corner criterion  
+
+% im: input image
+% tau: threshold
+
+% keyXs, keyYs: detected keypoints, with dimension [Number of keypoints] x [2]
+
+% YOUR CODE HERE
+  
+end
