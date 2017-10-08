4e36b51e97d60bd3b32a71923a7cb95b38d2b980
stephenlasky <stephenjlasky@gmail.com>
2017-10-08 17:03:34 -0400

    About to try and 'fix' the eigenvalue calculation.

--- prob_tracking/KLT_tracker.m
+++ prob_tracking/KLT_tracker.m
@@ -2,7 +2,7 @@ function im = KLT_tracker( im)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-% test code: 
+%%% test code: 
 % im = create_checkerboard(128);
 % im = imrotate(im,0);
 % imo = im;
@@ -21,15 +21,24 @@ H_mtxs = zeros(2,2,rows,cols,'single');
 w_size = 5;
 w_rad = floor(w_size/2);
 
+% compute Ix2, Iy2, Ixy
+Ix2 = grad_x .^2;
+Iy2 = grad_y .^2;
+Ixy = grad_x .* grad_y;
+
 % create filter for derivatives
 % filter = create_gaussian_filter(w_size);
 filter = 1; % keep filter at 1 for now
 for row = 1+w_rad:rows-w_rad
     for col = 1+w_rad:cols-w_rad
-        H(1,1) = sum(sum((filter .* grad_x(row-w_rad:row+w_rad,col-w_rad:col+w_rad)) .^2));
-        H(1,2) = sum(sum((filter .* grad_x(row-w_rad:row+w_rad,col-w_rad:col+w_rad)) .* (filter .* grad_y(row-w_rad:row+w_rad,col-w_rad:col+w_rad))));
+%         H(1,1) = sum(sum((filter .* grad_x(row-w_rad:row+w_rad,col-w_rad:col+w_rad)) .^2));
+%         H(1,2) = sum(sum((filter .* grad_x(row-w_rad:row+w_rad,col-w_rad:col+w_rad)) .* (filter .* grad_y(row-w_rad:row+w_rad,col-w_rad:col+w_rad))));
+%         H(2,1) = H(1,2);
+%         H(2,2) = sum(sum((filter .* grad_y(row-w_rad:row+w_rad,col-w_rad:col+w_rad)) .^2));
+        H(1,1) = sum(sum(filter.*Ix2(row-w_rad:row+w_rad,col-w_rad:col+w_rad)));
+        H(1,2) = sum(sum(filter.*Ixy(row-w_rad:row+w_rad,col-w_rad:col+w_rad)));
         H(2,1) = H(1,2);
-        H(2,2) = sum(sum((filter .* grad_y(row-w_rad:row+w_rad,col-w_rad:col+w_rad)) .^2));
+        H(2,2) = sum(sum(filter.*Iy2(row-w_rad:row+w_rad,col-w_rad:col+w_rad)));
         
         H_mtxs(1:2,1:2,row,col) = H(1:2,1:2);
     end
@@ -53,39 +62,38 @@ for row = 1+w_rad:rows-w_rad
     end
 end
 
-% 4. Find points with large responses (l_min > threshold)
-threshold = 0.0;
-im = zeros(rows,cols,'single');
-
-for row=1:rows
-    for col=1:cols
-        if lambda_sub(row,col) > threshold
-            im(row,col) = lambda_sub(row,col);
-        end
-    end
-end
-
-
-
-% % try exporting image as it is
-% % im = abs(lambda_add) .* abs(lambda_sub);
-% 
-% % try creating lambda_min, lambda_max arrays
-% lambda_min = lambda_sub;
-% lambda_max = lambda_add;
+% % 4. Find points with large responses (l_min > threshold)
+% threshold = 0.0;
+% im = zeros(rows,cols,'single');
 % 
 % for row=1:rows
 %     for col=1:cols
-%         if lambda_min(row,col) > lambda_max(row,col)
-%             % swap
-%             temp = lambda_min(row,col);
-%             lambda_min(row,col) = lambda_max(row,col);
-%             lambda_max(row,col) = temp;
+%         if lambda_sub(row,col) > threshold
+%             im(row,col) = lambda_sub(row,col);
 %         end
 %     end
 % end
-% 
-% im = lambda_add;
+
+
+% 5. Choose those points where lmin is a local maximum as features
+% for row = 1+w_rad:rows-w_rad
+%     for col = 1+w_rad:cols-w_rad
+%         window = im(row-w_rad:row+w_rad,col-w_rad:col+w_rad);
+%         max_val = max(window(:));
+%         window = max_val * floor(window / max_val);
+%         im(row-w_rad:row+w_rad,col-w_rad:col+w_rad) = window;
+%     end
+% end
+
+% TRY: 'R' method
+% R = det(M) - k(trace(M))^2
+% k = 0.04 - 0.06
+% R = l1*l2 - k * (l1+l2)^2
+k = 0.05;
+R = lambda_sub .* lambda_add;
+R = R - k * ((lambda_sub+lambda_add).^2);
+im = R;
+
 
 
 
--- prob_tracking/test.m
+++ prob_tracking/test.m
@@ -5,11 +5,17 @@
 
 
 
-% test code: 
-im = im2single(imread('images/hotel.seq0.png'));
-% im = imrotate(im,0);
+% %% test code: 
+im = create_checkerboard(64);
+im = imrotate(im,0);
 imo = im;
 im = KLT_tracker(im);
 figure(); imshow(cat(2,imo,im+0.5));
-%%
+% %%
+
+% filter = create_gaussian_filter(5);
+% figure(); imshow(filter+0.5);
+
+
+
 
